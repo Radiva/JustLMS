cb76740e6c81f3c5d82e7b1659396d99b6592aa2
Radiva <radiva.hera@gmail.com>
2025-10-21 09:45:18 +0700

    Removing old project files

--- resources/js/Pages/Admin/Index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>Admin Dashboard</h1>
-    <p>Halo, ini area khusus Admin 🚀</p>
-  </div>
-</template>
--- resources/js/Pages/User/Index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <h1>User Dashboard</h1>
-    <p>Halo, ini area khusus User 👤</p>
-  </div>
-</template>
